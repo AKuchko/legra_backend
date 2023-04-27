d65b624172f87fad19ddda091ca41b45cd0fb644
AK <101558236+AKuchko@users.noreply.github.com>
2023-04-27 15:16:15 +0700

    add: socket callback for client
    Added a callback that sends a new message to all clients that are on the comments page.

--- routes/chat.router.js
+++ routes/chat.router.js
@@ -15,7 +15,7 @@ router.get('/messages/:chat_id', async (req, res) => {
         const messages_query = 'SELECT message.*, user.user_name, user.profile_image\
                                 FROM message\
                                 INNER JOIN user\
-                                ON message.user_id = user.user_id AND message.chat_id = ?'
+                                ON message.user_id = user.user_id AND message.chat_id = ? ORDER BY message.created'
         const message_media_query = 'SELECT * FROM message_media WHERE message_id = ?'
 
         const chat_id = req.params.chat_id
@@ -53,23 +53,39 @@ router.post(
         try {
             const create_message = 'INSERT INTO message VALUES (?, ?, ?, ?, ?)' // (msg_id, chat_id, user_id, msg_text, created)
             const create_message_media = 'INSERT INTO message_media VALUES (?, ?, ?, ?)' // (media_id, msg_id, data, ext)
+            const get_user = 'SELECT user_name, profile_image FROM user WHERE user_id = ?'
 
             const { chat_id, message_text } = req.body
             const { message_media } = req.files
             const { user_id } = req.user
             const date = new Date().toISOString().slice(0, 19).replace('T', ' ')
-            
+
             if (!chat_id || (!message_text & !message_media)) return res.status(400).json({ err: 'empty message or chat not exist'})
+
             const new_msg = await database.query(create_message, [null, chat_id, user_id, message_text, date])
+            const [user_info] = await database.query(get_user, [user_id]);
+
+            const socket_msg = {
+                message_id: new_msg.insertId,
+                chat_id,
+                user_id,
+                message_text,
+                created: date,
+                user_name: user_info.user_name,
+                profile_image: ImageUtil.ConvertToBase64(user_info.profile_image, 'image/jpeg'),
+                media: [],
+            }
             
             const media_length = message_media ? message_media.length : 0
 
             for (let i = 0; i < media_length; i++){
-                console.log(message_media[i]);
-                const data = await sharp(message_media[i].buffer).toFormat('webp').webp({ lossless: false, quality: 50 }).toBuffer()
-                await database.query(create_message_media, [null, new_msg.insertId, data, 'image/webp'])
+                const data = await sharp(message_media[i].buffer).jpeg({ quality: 70, mozjpeg: true }).toBuffer()
+                const new_media = await database.query(create_message_media, [null, new_msg.insertId, data, 'image/jpeg'])
+                socket_msg.media.push({ data: ImageUtil.ConvertToBase64(data, 'image/jpeg'), media_id: new_media.insertId })
             }
 
+            console.log(socket_msg);
+            io.emit(`message:add:${chat_id}`, socket_msg)
             res.status(200).json({ ok: 'ok' })
         }
         catch (err) {
